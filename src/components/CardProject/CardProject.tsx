953562fb657e1d74ae6ad2227e1a1e128a50cd50
JoaoAlisonTI <joaoalison.ti@gmail.com>
2023-05-20 17:56:41 -0300

    Bug fixes

--- src/components/CardProject/CardProject.tsx
+++ src/components/CardProject/CardProject.tsx
@@ -1,18 +1,26 @@
 import { BsCloudArrowUp, BsGithub } from 'react-icons/bs'
 import Link from 'next/link'
+import Image from 'next/image'
 
 import styles from '@/components/CardProject/CardProject.module.css'
 
-export default function CardProject(props) {
-  const { imgSrc, projectName, projectDescription, linkOnline, linkGithub } = props;
+interface CardProjectProps {
+  imgSrc: string;
+  projectName: string;
+  projectDescription: string;
+  linkOnline: string;
+  linkGithub: string;
+}
+
+export default function CardProject(props: CardProjectProps) {
   
   return (
     
     <div className={styles.project}>
-    <img src="https://github.com/JoaoAlisonTI.png" alt="Imagem do projeto" />
+    <Image src="https://github.com/JoaoAlisonTI.png" alt="Imagem do projeto" width={265} height={180} />
     <div className={styles.info}>
-    <h4>{projectName}</h4>
-    <p>{projectDescription}</p>
+    <h4>{props.projectName}</h4>
+    <p>{props.projectDescription}</p>
     <Link className={styles.linkOn} href="{linkOnline}" target="_blank">
     <BsCloudArrowUp />
     </Link>
