b986e7610b609c3084a39f180538c4cfe3824ae3
JoaoAlisonTI <joaoalison.ti@gmail.com>
2023-05-23 20:21:56 -0300

    Fix css 3.9

--- src/components/CarouselCertifications/CarouselCertifications.tsx
+++ src/components/CarouselCertifications/CarouselCertifications.tsx
@@ -6,7 +6,7 @@ export default function CarouselCertifications() {
   return (
     <Carousel>
       <div>
-        <Image src="/certificado-angular.png" alt="Imagem 1" height={100} />
+        <Image src="/certificado-angular.png" alt="Imagem 1" height={120} />
       </div>
       <div>
         <Image src="/certificado-angular.png" alt="Imagem 2" height={80} />
