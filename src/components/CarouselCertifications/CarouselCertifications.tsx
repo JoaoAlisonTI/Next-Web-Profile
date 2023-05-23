19f7ba3401fa6a9759509e00de932f24c7c192be
JoaoAlisonTI <joaoalison.ti@gmail.com>
2023-05-23 19:51:42 -0300

    Css 3.4x

--- src/components/CarouselCertifications/CarouselCertifications.tsx
+++ src/components/CarouselCertifications/CarouselCertifications.tsx
@@ -6,18 +6,17 @@ export default function CarouselCertifications() {
   return (
     <Carousel>
       <div>
-        <Image src="/certificado.webp" alt="Imagem 1" height={140}
-                width={100} />
-        <p className="legend">Certificado Angular Developer</p>
+        <Image src="/certificado.webp" alt="Imagem 1" height={100}
+                width={80} />
       </div>
       <div>
         <Image src="/icon.png" alt="Imagem 2" height={100}
-                width={100} />
+                width={80} />
         <p className="legend">Legenda da imagem 2</p>
       </div>
       <div>
         <Image src="/icon.png" alt="Imagem 3" height={100}
-                width={100} />
+                width={80} />
         <p className="legend">Legenda da imagem 3</p>
       </div>
     </Carousel>
