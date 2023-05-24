45fad66653796b3f5e2ce86bcebfce0b030d7110
JoaoAlisonTI <joaoalison.ti@gmail.com>
2023-05-24 15:30:25 -0300

    Add css theme

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -1,15 +1,40 @@
 import { createTheme } from '@mui/material/styles';
 
-// Definindo as cores para os modos dark/light
 const darkTheme = createTheme({
   palette: {
     mode: 'dark',
+    primary: {
+      main: '#FF0000', // Cor primária no modo dark
+    },
+    secondary: {
+      main: '#00FF00', // Cor secundária no modo dark
+    },
+    background: {
+      default: '#121212', // Cor de fundo no modo dark
+      paper: '#1E1E1E', // Cor de fundo do papel no modo dark
+    },
+    text: {
+      primary: '#FFFFFF', // Cor do texto no modo dark
+    },
   },
 });
 
 const lightTheme = createTheme({
   palette: {
     mode: 'light',
+    primary: {
+      main: '#0000FF', // Cor primária no modo light
+    },
+    secondary: {
+      main: '#00FFFF', // Cor secundária no modo light
+    },
+    background: {
+      default: '#FFFFFF', // Cor de fundo no modo light
+      paper: '#F5F5F5', // Cor de fundo do papel no modo light
+    },
+    text: {
+      primary: '#000000', // Cor do texto no modo light
+    },
   },
 });
 
