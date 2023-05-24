5e87d336a7830ab97839293b447db3f8f9445397
JoaoAlisonTI <joaoalison.ti@gmail.com>
2023-05-23 22:02:08 -0300

    Add css 4.4

--- src/components/SkillsTech/SkillsTech.tsx
+++ src/components/SkillsTech/SkillsTech.tsx
@@ -6,13 +6,13 @@ export default function SkillsTech() {
     <>
       <div className={styles.container}>
         <div className={styles.tech}>
-          <Image src="/html5.svg" alt="Html5 Icon" height={10} width={10} />
+          <Image src="/html5.svg" alt="Html5 Icon" height={50} width={50} />
         </div>
         <div className={styles.tech}>
-          <Image src="/css3.svg" alt="Css3 Icon" />
+          <Image src="/css3.svg" alt="Css3 Icon" height={50} width={50} />
         </div>
         <div className={styles.tech}>
-          <Image src="/php.svg" alt="PHP Icon" />
+          <Image src="/php.svg" alt="PHP Icon" height={50} width={50} />
         </div>
       </div>
     </>
