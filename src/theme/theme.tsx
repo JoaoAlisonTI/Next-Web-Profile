892ac5a5845b17fc40ebcffb02452f07edfe5b8c
JoaoAlisonTI <joaoalison.ti@gmail.com>
2023-05-24 21:50:50 -0300

    Add new css theme 2.0

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -6,12 +6,9 @@ const darkTheme = createTheme({
     primary: {
       main: '#2b3c6a', // Cor primária no modo dark
     },
-    secondary: {
-      main: '#00FF00', // Cor secundária no modo dark
-    },
     background: {
       default: '#040600', // Cor de fundo no modo dark
-      paper: '#001D2C', // Cor de fundo do papel no modo dark
+      paper: '#040600', // Cor de fundo do papel no modo dark
     },
     text: {
       primary: '#f8f8f2', // Cor do texto no modo dark
@@ -22,12 +19,6 @@ const darkTheme = createTheme({
 const lightTheme = createTheme({
   palette: {
     mode: 'light',
-    primary: {
-      main: '#0000FF', // Cor primária no modo light
-    },
-    secondary: {
-      main: '#00FFFF', // Cor secundária no modo light
-    },
     background: {
       default: '#f3f1f1', // Cor de fundo no modo light
       paper: '#f3f1f1', // Cor de fundo do papel no modo light
