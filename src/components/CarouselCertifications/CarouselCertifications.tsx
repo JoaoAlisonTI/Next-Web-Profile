c3d36a767fe87aac9d31db983b9bc63f61a59c98
JoaoAlisonTI <joaoalison.ti@gmail.com>
2023-05-26 17:24:14 -0300

    Fix bug carrossel 3.0

--- src/components/CarouselCertifications/CarouselCertifications.tsx
+++ src/components/CarouselCertifications/CarouselCertifications.tsx
@@ -1,11 +1,12 @@
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import Image from 'next/image'
+import styles from '@/components/CarouselCertifications/CarouselCertifications.module.css'
 
 export default function CarouselCertifications() {
   return (
     <>
-    <Carousel style={{ zIndex: 0 }}>
+    <Carousel containerClass={styles.carouselContainer}>
       <div>
         <Image src="/certificado-angular.png" alt="Imagem 1" height={128} />
       </div>
