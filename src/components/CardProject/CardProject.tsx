7c7cb3272f3fde47641a9945d5b14eadd23aeaa3
JoaoAlisonTI <joaoalison.ti@gmail.com>
2023-05-27 19:27:30 -0300

    Add new css cardproject

--- src/components/CardProject/CardProject.tsx
+++ src/components/CardProject/CardProject.tsx
@@ -17,14 +17,14 @@ export default function CardProject(props: CardProjectProps) {
   return (
     <>
     <div className={styles.project}>
-    <Image src="{props.imgSrc}" width={265} height={180} alt="Imagem do projeto" />
+    <Image src={props.imgSrc} width={265} height={180} alt="Imagem do projeto" />
     <div className={styles.info}>
     <h4>{props.projectName}</h4>
     <p>{props.projectDescription}</p>
-    <Link className={styles.linkOn} href="{linkOnline}" target="_blank">
+    <Link className={styles.linkOn} href={linkOnline} target="_blank">
     <BsCloudArrowUp />
     </Link>
-    <Link className={styles.linkGit} href="{linkGithub}" target="_blank">
+    <Link className={styles.linkGit} href={linkGithub} target="_blank">
     <BsGithub />
     </Link>
     
