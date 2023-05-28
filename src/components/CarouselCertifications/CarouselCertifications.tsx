e802e9d4ab2cdb599fca3458751e31531d73a1b4
JoaoAlisonTI <joaoalison.ti@gmail.com>
2023-05-28 12:26:56 -0300

    Add new css 7.2

--- src/components/CarouselCertifications/CarouselCertifications.tsx
+++ src/components/CarouselCertifications/CarouselCertifications.tsx
@@ -9,10 +9,10 @@ export default function CarouselCertifications() {
     <div className={styles.carouselContainer}>
     <Carousel>
       <div>
-        <Image src="/certificados/certificado-angular.png" alt="Imagem 1" height={142} />
+        <Image src="/certificados/certificado-angular.png" alt="Imagem 1" height={160} />
       </div>
       <div>
-        <Image src="/certificados/certificado-angular.png" alt="Imagem 1" height={142} />
+        <Image src="/certificados/certificado-angular.png" alt="Imagem 1" height={160} />
       </div>
     </Carousel>
     </div>
