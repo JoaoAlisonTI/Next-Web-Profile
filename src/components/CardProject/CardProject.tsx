bb52d2031cc02cb0b74c5a34d39c398b280407ff
JoaoAlisonTI <joaoalison.ti@gmail.com>
2023-05-29 16:34:53 -0300

    Fix bug 2.1

--- src/components/CardProject/CardProject.tsx
+++ src/components/CardProject/CardProject.tsx
@@ -13,30 +13,31 @@ interface CardProjectProps {
 }
 
 export default function CardProject(props: CardProjectProps) {
-  const linkGH = linkGithub!;
+  const linkGH = props.linkGithub!;
   
   return (
     <>
-    <div className={styles.project}>
-    <Image className={styles.imgProject} src={props.imgSrc} height={170} alt="Imagem do projeto" />
-    
-    <div className={styles.info}>
-    <h4>{props.projectName}</h4>
-    <p>{props.projectDescription}</p>
-    
-    <div className={styles.contentLinks}>
-    <Link href={props.linkOnline} target="_blank">
-    <button className={styles.btnOnline}>
-      Acesse online
-    </button>
-    </Link>
-    <Link className={styles.linkGit} href={linkGH} target="_blank">
-    <BsGithub />
-    </Link>
-    </div>
-    
-    </div>
-    </div>
+      <div className={styles.project}>
+        <Image className={styles.imgProject} src={props.imgSrc} height={170} alt="Imagem do projeto" />
+        
+        <div className={styles.info}>
+          <h4>{props.projectName}</h4>
+          <p>{props.projectDescription}</p>
+          
+          <div className={styles.contentLinks}>
+            <Link href={props.linkOnline} target="_blank">
+              <button className={styles.btnOnline}>
+                Acesse online
+              </button>
+            </Link>
+            {props.linkGithub && (
+              <Link className={styles.linkGit} href={linkGH} target="_blank">
+                <BsGithub />
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
     </>
-    )
+  );
 }
