5c6d34137baf538ac8c86c511e81fb80a1267ca7
JoaoAlisonTI <joaoalison.ti@gmail.com>
2023-05-29 16:06:41 -0300

    Add new project

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -11,18 +11,17 @@ export default function ProjectsPage() {
       <h1>Meus projetos</h1>
       <div className={styles.content}>
         <CardProject 
-          imgSrc="/projetos/projeto-pokedex.png"
-          projectName="Aplicação Web Pokédex"
-          projectDescription="Esse é apenas um card do projeto para demonstração, para saber como será a versão final."
-          linkOnline="nada aqui"
-          linkGithub="nada aqui"
+          imgSrc="/projetos/sibi-gj.png"
+          projectName="Aplicação web SIBI - Gerardo José"
+          projectDescription="É um projeto comunitário para nossa antiga escola, onde me juntei com meus colegas (Kayke, Marquinhos, Romário e Brunno) para desenvolvermos um sistema de cadastro, listagem e requisição dos livros da biblioteca. Fiquei responsável pela tela de listagem/pesquisa dos livros e da tela de edição. Techs: HTML/CSS, PHP e SQL. Como o projeto só pode ser acessado com login o link online é de uma LP com imagens e detalhes do sistema."
+          linkOnline="https://sibi.vercel.app/"
         />
         <CardProject 
-          imgSrc="/icons/reactjs.svg"
-          projectName="Projeto Exemplo"
-          projectDescription="Esse é apenas um card do projeto para demonstração, para saber como será a versão final."
-          linkOnline="nada aqui"
-          linkGithub="nada aqui"
+          imgSrc="/projetos/projeto-pokedex.png"
+          projectName="Aplicação Web Pokédex"
+          projectDescription="Pokédex é uma enciclopédia virtual que detém todas as espécies de Pokémon, ou seja, desde a 1° geração até a última geração. Techs: HTML/CSS, JavaScript e API. Para mais detalhes acesse o repositório no botão direito"
+          linkOnline="https://joaoalisonti.github.io/Pokedex-Js/"
+          linkGithub="https://github.com/JoaoAlisonTI/Pokedex-Js"
         />
         </div>
       </section>  
