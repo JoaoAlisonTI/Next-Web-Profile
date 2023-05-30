2f012d3ddcdcd96f9855aa5fe998a997331f7be2
JoaoAlisonTI <joaoalison.ti@gmail.com>
2023-05-30 15:19:15 -0300

    Add new projects, imc calc and habits

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -15,11 +15,13 @@ export default function ProjectsPage() {
           projectName="Aplicação Web SIBI - Gerardo José"
           projectDescription="É um projeto comunitário para nossa antiga escola, onde me juntei com meus colegas (Kayke, Marquinhos, Romário e Brunno) para desenvolvermos um sistema de cadastro, listagem e requisição dos livros da biblioteca. Fiquei responsável pela tela de listagem/pesquisa dos livros e da tela de edição. Techs: HTML/CSS, PHP e SQL. Como o projeto só pode ser acessado com login o link online é de uma LP com imagens e detalhes do sistema."
           linkOnline="https://sibi.vercel.app/"
+          linkGithub="https://github.com/JoaoAlisonTI/Page-Livros"
         />
         <CardProject 
           imgSrc="/projetos/buzzfeed.jpg"
           projectName="Aplicação Web BuzzFeed"
           projectDescription="BuzzFeed é um quiz de perguntas onde dependendo das opções/respostas que são escolhidas, definirá o seu resultado. Em específico, se você será um heroi ou vilão. Para o armazenamento das perguntas e respostas foi criado um data fake em json. Techs: Angular e Typescript. "
+          linkOnline="https://buzzfeed-quizz.vercel.app/"
           linkGithub="https://github.com/JoaoAlisonTI/Buzzfeed-Quizz-Project"
         />
         <CardProject 
@@ -28,6 +30,20 @@ export default function ProjectsPage() {
           projectDescription="Pokédex é uma enciclopédia virtual que detém todas as espécies de Pokémon, ou seja, desde a 1° geração até a última geração. Techs: HTML/CSS, JavaScript e API. Para mais detalhes acesse o repositório no botão direito."
           linkOnline="https://joaoalisonti.github.io/Pokedex-Js/"
           linkGithub="https://github.com/JoaoAlisonTI/Pokedex-Js"
+        />
+        <CardProject 
+          imgSrc="/projetos/habits.jpg"
+          projectName="Aplicação Web Habits Tracker"
+          projectDescription="o Habits foi desenvolvido durante a NLW 11, um evento exclusivo e gratuito promovido pela Rocketseat para ensino de tecnologias WEB. O Habits Tracker é um app para ajudar a rastrear os hábitos, crie um novo dia e faça a gestão de hábitos diários. Após criar um novo dia e marcar um hábito como concluído as informações serão guardadas no LocalStorage do navegador do seu dispositivo. Techs: HTML/CSS e JavaScript."
+          linkOnline="https://joaoalisonti.github.io/NLW-11--SETUP/"
+          linkGithub="https://github.com/JoaoAlisonTI/NLW-11--SETUP"
+        />
+        <CardProject 
+          imgSrc="/projetos/imc-calc.jpg"
+          projectName="Aplicação Web Calculadora de IMC"
+          projectDescription='O projeto pede para o usuário sua altura e peso (utilize vírgula), com isso é só clicar em calcular para ser realizado o cálculo do IMC ("IMC = peso / (altura x altura)"). Finalmente será exibido a tabela com seu Índice de Massa Corpórea. Desenvolvi com o intuito de treinar minhas habilidades, aprender novas funcionalidades do JavaScript e conseguir reutilizar o código para futuros projetos. Techs: HTML/CSS e JavaScript.'
+          linkOnline="https://joaoalisonti.github.io/IMC-Calc/"
+          linkGithub="https://github.com/JoaoAlisonTI/IMC-Calc"
         />
         </div>
       </section>  
