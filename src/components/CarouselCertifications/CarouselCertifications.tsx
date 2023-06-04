76c1819395dd4e2361a2017f17d8a2f4df27ca18
JoaoAlisonTI <joaoalison.ti@gmail.com>
2023-06-04 16:13:51 -0300

    Fix bug medias queries 3.0

--- src/components/CarouselCertifications/CarouselCertifications.tsx
+++ src/components/CarouselCertifications/CarouselCertifications.tsx
@@ -9,10 +9,10 @@ export default function CarouselCertifications() {
     <div className={styles.carouselContainer}>
     <Carousel>
       <div>
-        <Image src="/certificados/certificado-angular.png" alt="Imagem 1" height={160} />
+        <Image src="/certificados/certificado-dsm-min.png" alt="Imagem 1" height={170} />
       </div>
       <div>
-        <Image src="/certificados/certificado-dsm-min.png" alt="Imagem 1" height={160} />
+        <Image src="/certificados/certificado-angular.png" alt="Imagem 1" height={170} />
       </div>
     </Carousel>
     </div>
