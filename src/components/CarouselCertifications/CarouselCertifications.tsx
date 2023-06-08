9bca6c67ddfdd0bba1efc7c096684f63aa70426d
JoaoAlisonTI <joaoalison.ti@gmail.com>
2023-06-08 18:17:11 -0300

    Fix bug certifications

--- src/components/CarouselCertifications/CarouselCertifications.tsx
+++ src/components/CarouselCertifications/CarouselCertifications.tsx
@@ -7,7 +7,7 @@ export default function CarouselCertifications() {
   return (
     <>
     <div className={styles.carouselContainer}>
-    <Carousel infiniteLoop={true} autoPlay={true} showThumbs={true} showStatus={false} interval={3000} showIndicators={false}>
+    <Carousel infiniteLoop={true} autoPlay={true} showThumbs={true} showStatus={false} interval={3000} showIndicators={false} height={186}>
       <div>
         <Image src="/certificados/certificado-dsm-min.png" alt="Certificado Introdução ao Desenvolvimento Moderno de Software" height={186} />
         <div><h4>Introdução ao Desenvolvimento Moderno de Software</h4></div>
