86bac7d8e0d378a7e47b601eb34c944191c70da3
JoaoAlisonTI <joaoalison.ti@gmail.com>
2023-06-08 18:20:45 -0300

    Delete property height

--- src/components/CarouselCertifications/CarouselCertifications.tsx
+++ src/components/CarouselCertifications/CarouselCertifications.tsx
@@ -6,7 +6,7 @@ import styles from '@/components/CarouselCertifications/CarouselCertifications.m
 export default function CarouselCertifications() {
   return (
     <>
-    <div className={styles.carouselContainer} height={186}>
+    <div className={styles.carouselContainer}>
     <Carousel infiniteLoop={true} autoPlay={true} showThumbs={true} showStatus={false} interval={3000} showIndicators={false}>
       <div>
         <Image src="/certificados/certificado-dsm-min.png" alt="Certificado Introdução ao Desenvolvimento Moderno de Software" height={186} />
