8e9a98de99676b23de109337bf43e4bf9375f443
JoaoAlisonTI <joaoalison.ti@gmail.com>
2023-06-08 21:20:39 -0300

    Add new carousel  with material ui

--- src/components/CarouselCertifications/CarouselCertifications.tsx
+++ src/components/CarouselCertifications/CarouselCertifications.tsx
@@ -1,31 +1,52 @@
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import React, { useState } from 'react';
+import { Box, Typography } from '@mui/material';
+
 import Image from 'next/image'
-import styles from '@/components/CarouselCertifications/CarouselCertifications.module.css'
 
 export default function CarouselCertifications() {
+  
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const images = [
+    {
+      src: '/certificado-angular.png',
+      description: 'Potência Tech Angular Developer - Powered by iFood',
+    },
+    {
+      src: '/certificado-dsm-min.png',
+      description: 'Introdução ao Desenvolvimento Moderno de Software',
+    },
+    {
+      src: '/js0-trybe.png',
+      description: 'JavaScript do zero',
+    },
+    {
+      src: '/hero-code.jpg',
+      description: 'Workshop Semanda do Herói - Hero Code',
+    },
+  ];
+
+  const handleChangeImage = (index) => {
+    setCurrentImage(index);
+  };
+  
   return (
     <>
-    <div className={styles.carouselContainer}>
-    <Carousel infiniteLoop={true} autoPlay={true} showThumbs={true} showStatus={false} interval={3000} showIndicators={false}>
-      <div>
-        <Image src="/certificados/certificado-dsm-min.png" alt="Certificado Introdução ao Desenvolvimento Moderno de Software" height={186} />
-        <div><h4>Introdução ao Desenvolvimento Moderno de Software</h4></div>
-      </div>
-      <div>
-        <Image src="/certificados/certificado-angular.png" alt="Certificado Potência Tech Angular Developer" height={186} />
-        <div><h4>Potência Tech Angular Developer - Powered by iFood</h4></div>
-      </div>
-      <div>
-        <Image src="/certificados/js0-trybe.png" alt="Certificado JavaScript do zero" height={186} />
-        <div><h4>JavaScript do zero</h4></div>
-      </div>
-      <div>
-        <Image src="/certificados/hero-code.jpg" alt="Certificado do Workshop Semana do Herói - Hero Code" height={186} />
-        <div><h4>Workshop Semana do Herói - Hero Code</h4></div>
-      </div>
-    </Carousel>
-    </div>
+    <Box>
+      <Image src={images[currentImage].src} alt={`Imagem do certificado ${currentImage + 1}`} />
+
+      <Box>
+        {images.map((image, index) => (
+          <Typography
+            key={index}
+            onClick={() => handleChangeImage(index)}
+            style={{ cursor: 'pointer', fontWeight: currentImage === index ? 'bold' : 'normal' }}
+          >
+            {image.description}
+          </Typography>
+        ))}
+      </Box>
+    </Box>
     </>
   )
 }
